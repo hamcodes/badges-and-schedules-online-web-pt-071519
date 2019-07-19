c3c25c38514b61f301ce1c9b3e300bd23d19800d
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-19 17:31:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(names)
+  return "Hello, my name is #{names}."
+end
+
+def batch_badge_creator(names)
+  names.collect {|name| badge_maker(name)}
+end
+
+def assign_rooms(list)
+  list.each_with_index.map {|person, index|
+  "Hello, #{person}! You'll be assigned to room #{index +1}!"}
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do 
+end
